9468acb2bc048452e52002beff9c6ab3b3119639
Levis Sikudi <levisikudi@gmail.com>
2023-01-09 18:20:50 -0500

    added body-parser dependency and temporary post response

--- calculator.js
+++ calculator.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 
 const app = express()
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.get('/',(req, res) =>{
     res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) =>{
+    res.send('Thank for posting that')
 })
 
 app.listen(3000, () => {
